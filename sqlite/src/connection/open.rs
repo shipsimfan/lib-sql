f5ad84c78faee9175c7bdd88328cb08f02a538f3
Lance Hart <me@lancehart.ca>
2025-04-03 17:34:58 -0400

    Added enabling of foreign keys on new connections

--- sqlite/src/connection/open.rs
+++ sqlite/src/connection/open.rs
@@ -1,18 +1,19 @@
-use crate::SQLite3Connection;
+use crate::{SQLite3Connection, SQLite3ExecuteError};
+use sql::Connection;
 use sqlite3::{
-    sqlite3_close, sqlite3_open_v2, try_sqlite3, SQLiteError, SQLITE_OPEN_CREATE,
-    SQLITE_OPEN_NOMUTEX, SQLITE_OPEN_READWRITE,
+    sqlite3_close, sqlite3_open_v2, try_sqlite3, SQLITE_OPEN_CREATE, SQLITE_OPEN_NOMUTEX,
+    SQLITE_OPEN_READWRITE,
 };
 use std::{path::Path, ptr::null_mut};
 
 impl SQLite3Connection {
     /// Attempts to open the database at `path`
-    pub fn open<P: AsRef<Path>>(path: P) -> Result<Self, SQLiteError> {
+    pub fn open<P: AsRef<Path>>(path: P) -> Result<Self, SQLite3ExecuteError> {
         let mut path = path.as_ref().as_os_str().as_encoded_bytes().to_vec();
         path.push(0);
 
         let mut handle = null_mut();
-        try_sqlite3!(sqlite3_open_v2(
+        let mut connection = try_sqlite3!(sqlite3_open_v2(
             path.as_ptr().cast(),
             &mut handle,
             SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE | SQLITE_OPEN_NOMUTEX,
@@ -21,7 +22,11 @@ impl SQLite3Connection {
         .map(|_| SQLite3Connection { handle })
         .map_err(|error| {
             try_sqlite3!(sqlite3_close(handle)).unwrap();
-            error
-        })
+            SQLite3ExecuteError::new(error.to_string())
+        })?;
+
+        connection.execute("PRAGMA foreign_keys = ON;")?;
+
+        Ok(connection)
     }
 }
--- sqlite/src/pool/open.rs
+++ sqlite/src/pool/open.rs
@@ -1,10 +1,9 @@
-use crate::{SQLite3Connection, SQLite3ConnectionPool};
-use sqlite3::SQLiteError;
+use crate::{SQLite3Connection, SQLite3ConnectionPool, SQLite3ExecuteError};
 use std::{path::Path, sync::Mutex};
 
 impl SQLite3ConnectionPool {
     /// Attempts to open the database at `path`
-    pub fn open<P: AsRef<Path>>(path: P) -> Result<Self, SQLiteError> {
+    pub fn open<P: AsRef<Path>>(path: P) -> Result<Self, SQLite3ExecuteError> {
         SQLite3Connection::open(path).map(|connection| SQLite3ConnectionPool {
             connection: Mutex::new(connection),
         })
